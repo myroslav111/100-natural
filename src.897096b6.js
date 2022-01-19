8a8e3c18804940588e72992c74a0059457d2381f
myroslav111 <92175747+myroslav111@users.noreply.github.com>
2022-01-19 08:44:28 +0000

    Deploying to gh-pages from @ myroslav111/100-natural@afd4033dd3c2ab4997764d41ce7ff965f9b6e3f9 🚀

--- src.897096b6.js
+++ src.897096b6.js
@@ -5,4 +5,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss"),require("./js/burger-menu");
 },{"./sass/main.scss":"clu1","./js/burger-menu":"w7zl"}]},{},["Focm"], null)
-//# sourceMappingURL=/100-natural/src.60b03e2b.js.map
+//# sourceMappingURL=/100-natural/src.897096b6.js.map
