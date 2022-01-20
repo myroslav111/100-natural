93550cbdd4c6fbf1a00c87c577d8232b171875b8
myroslav <myrkozar@gmail.com>
2022-01-20 15:36:48 +0200

    max
    max

--- src/js/burger-menu.js
+++ src/js/burger-menu.js
@@ -9,3 +9,21 @@
         burger.classList.toggle('is-hidden');
     });
 })()
+// (() => {
+//     const menuBurger = document.querySelector('.header__burger');
+//     const mobMenu = document.querySelector('.mob-menu');
+//     const burger = document.querySelector('.burger');
+//     const menuItem = document.querySelectorAll('.mobileMenu__item');
+//     menuBurger.addEventListener('click', () => {
+//         mobMenu.classList.toggle('is-hidden');
+//         menuBurger.classList.toggle('is-hidden');
+//         burger.classList.toggle('is-hidden');
+//     });
+//     menuItem.forEach(function(item){
+//       item.addEventListener('click', function(e) {
+//          mobMenu.classList.toggle('is-hidden');
+//         menuBurger.classList.toggle('is-hidden');
+//         burger.classList.toggle('is-hidden');
+//       });
+//    });
+// })()
